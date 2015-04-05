830c99b2cf04c22397ba6d3167d73fa939059acb
ankur singh <ankur13019@iiitd.ac.in>
2015-04-06 04:11:45 +0530

    Added nested attributes for standard subject

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -11,4 +11,5 @@
 class Standard < ActiveRecord::Base
   has_many :subjects
   validates_presence_of :name, on: :create, message: "can't be blank"
+  accepts_nested_attributes_for :subjects, reject_if: ->(attributes){ attributes['name'].blank? },allow_destroy: true
 end
